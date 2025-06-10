7c1a69b7d5b62dd7d269e8aad4d58c6a8b7da16f
hfmjunio <haniejunio19@gmail.com>
2025-06-10 22:30:27 +0800

    first commit

---
+++ src/WPButton.tsx
@@ -0,0 +1,14 @@
+function WelcomePageButton () {
+
+    return(
+
+      <a href="signup.html">
+        <div className="wpbutton">
+      <button className="proceedbtn">Proceed</button>
+      </div> </a>
+    );
+
+
+}
+
+export default WelcomePageButton
